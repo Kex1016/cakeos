0650628933ef0ffd39eb73777f2fc15afa24e12a
cakes <cakes@haiiro.moe>
2026-05-06 21:17:00 +0200

    chore: configure CachyOS kernel and add binary cache

--- systems/universe/configuration.nix
+++ systems/universe/configuration.nix
@@ -16,6 +16,11 @@
     #../../modules/system/apps/mongo.nix
   ];
 
+  nix.settings.substituters = [ "https://attic.xuyh0120.win/lantian" ];
+  nix.settings.trusted-public-keys = [ "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc=" ];
+
+  boot.kernelPackages = pkgs.cachyosKernels.linuxPackages-cachyos-latest;
+
   services.flatpak.enable = true;
   programs.appimage = {
     enable = true;
