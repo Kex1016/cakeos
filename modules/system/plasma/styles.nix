70e4851e82f5dbeffc3b6dafd76b70cdd66575a2
cakes <cakes@haiiro.moe>
2026-05-10 08:55:13 +0000

    feat: add kvantum

--- modules/system/plasma/styles.nix
+++ modules/system/plasma/styles.nix
@@ -4,5 +4,6 @@
     inputs.kwin-effects-glass.packages.${pkgs.system}.default
     inputs.glass.packages.${pkgs.system}.glass-qt5
     inputs.glass.packages.${pkgs.system}.glass-qt6
+    pkgs.kdePackages.qtstyleplugin-kvantum
   ];
 }
