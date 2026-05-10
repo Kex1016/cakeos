c69616308601803a19ba510ae95aa7081828aa6e
cakes <cakes@haiiro.moe>
2026-05-10 08:49:28 +0000

    fix: enable libinput
    not sure if this is a fix or not actually, but i had small issues and this fixed(?) them

--- modules/system/plasma/wm.nix
+++ modules/system/plasma/wm.nix
@@ -4,6 +4,7 @@
     desktopManager.plasma6.enable = true;
     displayManager.sddm.enable = true;
     displayManager.sddm.wayland.enable = true;
+    libinput.enable = true;
   };
 
   environment.systemPackages = with pkgs; [
