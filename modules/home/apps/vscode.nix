d87efbdf6dcc24b957be0a67beb02d61ba4ad9bd
cakes <cakes@haiiro.moe>
2026-06-28 10:53:34 +0200

    fix: rename programs.vscode to programs.vscodium

--- modules/home/apps/vscode.nix
+++ modules/home/apps/vscode.nix
@@ -3,7 +3,7 @@
 {
   home.packages = [ pkgs.biome ];
 
-  programs.vscode = {
+  programs.vscodium = {
     enable = true;
     package = pkgs.vscodium;
     profiles.default.extensions = with pkgs.vscode-extensions; [
