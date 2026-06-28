2f4d4f76f28a864e704c80e36833b5249057011e
cakes <cakes@haiiro.moe>
2026-06-28 17:56:06 +0200

    feat: add the oxygen theme (hopefully)

--- modules/system/plasma/styles.nix
+++ modules/system/plasma/styles.nix
@@ -1,9 +1,9 @@
 { pkgs, inputs, ... }:
 {
   environment.systemPackages = [
-    inputs.kwin-effects-glass.packages.${pkgs.system}.default
-    inputs.glass.packages.${pkgs.system}.glass-qt5
-    inputs.glass.packages.${pkgs.system}.glass-qt6
     pkgs.kdePackages.qtstyleplugin-kvantum
+    kdePackages.oxygen
+    kdePackages.oxygen-icons
+    kdePackages.oxygen-sounds
   ];
 }
